64180cba00eda274781f36aee2ad647b13ac9841
test <k.widlak@microsoft.wsei.edu.pl>
2019-02-04 17:50:51 +0100

    metody rozszerzające

---
+++ ExtensionMethods/Extensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class Extensions
+    {
+
+        public static int ToInt(this String val)
+        {
+            try
+            {
+                return Convert.ToInt32(val);
+            }
+            catch (Exception ex)
+
+            {
+                return -1;
+            }
+        }
+
+        public static int ToInt1(String val)
+        {
+            try
+            {
+                return Convert.ToInt32(val);
+            }
+            catch (Exception ex)
+
+            {
+                return -1;
+            }
+        }
+
+        public static string Left(this string val, int digits = 1)//this -> do jakiej klasy chcemy się odniesc (klasy string)
+        { 
+            return val.Substring(0, digits);
+        }
+
+        public static void Log (this string text)
+        {
+            try
+            {
+                string directory = "D:/Loggs";
+
+                File.AppendAllText(Path.Combine(directory, "Info.txt"),
+                    $"{Environment.NewLine} Data: {DateTime.Now.ToShortDateString()} Informacja: {text}");
+            }
+            catch (DirectoryNotFoundException dnfe)
+            {
+                //LogError(dnfe.Message);
+            }
+            catch (Exception)
+            {
+              //  LogError("Coś sie zepsuło");
+            }
+        }
+
+
+    }
+}
--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -10,6 +10,33 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
+
+            string number = "3";
+            int n = Extensions.ToInt(number);
+            int n2 = number.ToInt(); // metoda rozszerzająca - > nie podajemy parametru "this"; kazdy string ma metodę toint
+
+            int n1 = Extensions.ToInt(number);
+            string z = 20.ToString();
+
+
+            string text = "kasia ma psa";
+
+            text.Log();
+            try
+            {
+                throw new Exception("blabla");
+            }
+            catch (Exception exe)
+            {
+                exe.Message.Log();
+         
+            }
+            Console.WriteLine(text.Left(5));
+            Console.WriteLine(n);
+            Console.WriteLine(n2);
+            Console.Read();
+
+
         }
     }
 }
